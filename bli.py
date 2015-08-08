b9802a1507c658c47f61dc2799d0a61c62f3e0ab
irbanoz <irbyjm@gmail.com>
2015-08-08 01:16:17 +0000

    Handles folder validation and stdout parsing; resolves #4

--- bli.py
+++ bli.py
@@ -6,7 +6,7 @@ import subprocess
 
 # definitions
 bli_path = "/".join(sys.argv[0].split("/")[0:-1])
-policies = ["phys"]
+policies = ["phys", "virt"]
 
 # defaults
 sensor_file = "sensor.csv"
@@ -173,6 +173,7 @@ def check_policy(sensors):
 		policy = {}
 		for pol in policies:
 			policy[pol] = {}
+			policy[pol]['error'] = False
 			stdout = subprocess.Popen(
 				["find", os.path.join(bli_path, "deploy", pol, "site"), "-exec", "md5sum", "{}", ";"],
 				stdout = subprocess.PIPE,
@@ -185,19 +186,30 @@ def check_policy(sensors):
 				if len(line) == 2:
 					line[1] = line[1].split(os.path.join(bli_path, "deploy", pol, "site/"))[1]
 					policy[pol][line[1]] = line[0]
+				else:
+					if "No such file" in ' '.join(line):
+						policy[pol]['error'] = True
 
-			print "\n{0:15s} : {1:20s} : {2:6s} : {3:8s} : {4}".format("IP Address", "Hostname", "Policy", "Issue", "File")
-			print "-"*120
+		print "\n{0:15s} : {1:20s} : {2:6s} : {3:8s} : {4}".format("IP Address", "Hostname", "Policy", "Issue", "File/Details")
+		print "-"*120
 
-			for sensor in sorted(sensors):
-				if "Error" not in sensors[sensor]['status']:
-					print "{0:15s} : {1:20s} : {2:6s} : {3:8s} :".format(sensor, sensors[sensor]['hostname'], sensors[sensor]['policy_type'], "")
-					for policy_file in policy[sensors[sensor]['policy_type']]:
-						if policy_file in sensors[sensor]['policy_file']:
-							if policy[sensors[sensor]['policy_type']][policy_file] != sensors[sensor]['policy_file'][policy_file]:
-								print "{0:15s} : {1:20s} : {2:7s}: {3:8s} : {4} ".format("", "", "", "modified", policy_file)
-						else:
-							print "{0:15s} : {1:20s} : {2:7s} : {3:8s} : {4} ".format("", "", "", "missing", policy_file)
+		for sensor in sorted(sensors):
+			if "Error" not in sensors[sensor]['status']:
+				print "{0:15s} : {1:20s} : {2:6s} : {3:8s} :".format(sensor, sensors[sensor]['hostname'], sensors[sensor]['policy_type'], "")
+
+				if policy[sensors[sensor]['policy_type']]['error'] == False:
+					if sensors[sensor]['policy_type'] in policies:
+						for policy_file in policy[sensors[sensor]['policy_type']]:
+							if policy_file in sensors[sensor]['policy_file']:
+								if policy[sensors[sensor]['policy_type']][policy_file] != sensors[sensor]['policy_file'][policy_file]:
+									print "{0:15s} : {1:20s} : {2:7s}: {3:8s} : {4} ".format("", "", "", "modified", policy_file)
+							else:
+								if policy_file != "error":
+									print "{0:15s} : {1:20s} : {2:7s} : {3:8s} : {4} ".format("", "", "", "missing", policy_file)
+					else:
+						print "{0:15s} : {1:20s} : {2:6s} : {3:8s} : {4} ".format("", "", "", "error", "policy '"+sensors[sensor]['policy_type']+"' not defined in bli configuration")
+				else:
+					print "{0:15s} : {1:20s} : {2:6s} : {3:8s} : {4} ".format("", "", "", "error", "deployment information for policy '"+sensors[sensor]['policy_type']+"' does not exist")
 	else:
 		print "\nPolicy validation unavailable due to lack of deployment data"
 
